a0cd47c727c8f6f9b9753b0797fca566c1692609
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2020-07-04 08:31:35 +0300

    Implemented 3 stage - Let’s count!

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -2,6 +2,7 @@ package bot
 
 import java.util.*
 
+
 fun main() {
     val scanner = Scanner(System.`in`)
 
@@ -9,17 +10,24 @@ fun main() {
     println("I was created in 2020.")
     println("Please, remind me your name.")
 
-    val name = scanner.next()
+    val name = scanner.nextLine()
 
     println("What a great name you have, $name!")
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
 
-    val remainder3 = scanner.nextInt()
-    val remainder5 = scanner.nextInt()
-    val remainder7 = scanner.nextInt()
-
-    val age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+    val rem3 = scanner.nextInt()
+    val rem5 = scanner.nextInt()
+    val rem7 = scanner.nextInt()
+    val age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
     println("Your age is $age; that's a good time to start programming!")
+    println("Now I will prove to you that I can count to any number you want.")
+
+    val n = scanner.nextInt()
+    for (i in 0..n) {
+        println("$i!")
+    }
+
+    println("Completed, have a nice day!")
 }

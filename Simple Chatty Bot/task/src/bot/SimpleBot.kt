2153ed9d4672098328886183fb9b4cc7a04276cf
Nikita Krivosheyev <jast.e.new.name@gmail.com>
2020-07-04 07:36:46 +0300

    Implemented 2 stage - How old are you?

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -1,6 +1,25 @@
 package bot
 
+import java.util.*
+
 fun main() {
+    val scanner = Scanner(System.`in`)
+
     println("Hello! My name is Aid.")
     println("I was created in 2020.")
+    println("Please, remind me your name.")
+
+    val name = scanner.next()
+
+    println("What a great name you have, $name!")
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    val remainder3 = scanner.nextInt()
+    val remainder5 = scanner.nextInt()
+    val remainder7 = scanner.nextInt()
+
+    val age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+    println("Your age is $age; that's a good time to start programming!")
 }
